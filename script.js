0c133c9b9e20511d9c909b0be9e3a555f8f37809
ivanf-12 <febriantoivan12@gmail.com>
2022-05-24 16:08:55 +0700

    finish calculator design

---
+++ script.js
@@ -0,0 +1,36 @@
+const grid = document.getElementById('buttons');
+
+//math operators
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//functions
+
+function operate(operator, a, b) {
+  if(operator == '+') {
+    return add(a, b);
+  }
+  else if(operator == '-') {
+    return subtract(a, b);
+  }
+  else if(operator == '*') {
+    return multiply(a, b);
+  }
+  else {
+    return divide(a, b);
+  }
+}
